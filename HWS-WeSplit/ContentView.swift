4f88ea54d507fe11fcbdcaf4bced652a4e58a84a
Michael Bartee <michaelbartee@gmail.com>
2021-02-05 13:07:28 -0500

    Initial Commit

---
+++ HWS-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS-WeSplit
+//
+//  Created by Michael Bartee on 2/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS-WeSplit/HWS_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_WeSplitApp.swift
+//  HWS-WeSplit
+//
+//  Created by Michael Bartee on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
